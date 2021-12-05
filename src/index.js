1d970d973bac8f11fddbd2476c7ebba44edeb898
Patrick Brennan <pbrennanmusic@gmail.com>
2021-12-05 15:04:04 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={console.log("It worked!")} />
   </div>,
   document.getElementById('root')
 );
